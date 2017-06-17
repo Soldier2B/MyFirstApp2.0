5215caa3ed6994f67293112dee1d3beab9403632
Oliver Klein <Oliver@Olivers-MacBook-Pro.local>
2017-06-17 01:08:20 -0300

    Add functions to code

--- MyFirstApp2.0/main.cpp
+++ MyFirstApp2.0/main.cpp
@@ -1,49 +1,93 @@
 
-// First game attempt, 1d humans vs Skelletons
+// First game attempt, 1d humans vs Skeletons
 
 #include <iostream>
 #include <string>
 #include <random>
 #include <ctime>
 
-
 using namespace std;
+
+// Forward Declarations
+
+int getHumans();
+int getSkeletons();
+void battle(int humans, int skeletons);
+void printResults(int humans, int skeletons);
+
 
 int main()
 {
     //variables and other things needed
     
-    int skelletons;
+    int skeletons;
     int humans;
+    
+    //interface
+    
+    cout << "interface\n";
+    
+    //intro
+    //define number of skely and humans
+    
+    cout << "Hello, welcome to humans vs skeletons.\n";
+    
+    humans = getHumans();
+
+    skeletons = getSkeletons();
+
+    cout << humans <<" humans will fight against " << skeletons << " skeletons.\n";
+    cout << "Let the battle start!!!\n";
+    
+    //battle
+    
+    battle(humans, skeletons);
+    
+    cout << "Wave 2?\n" << "How many more skeletons do they fight?\n";
+    cin >> skeletons;
+    
+    battle(humans, skeletons);
+
+    //outro
+    
+    printResults (humans, skeletons);
+  
+    
+    exit (0);
+    
+}
+
+//Functions
+
+int getHumans() {
+    int humans;
+    cout << "Please select the number of humans.\n";
+    cin >> humans;
+    return humans;
+}
+
+
+int getSkeletons() {
+    int skeletons;
+    cout << "Now, select the number of skeletons they will fight against.\n";
+    cin >> skeletons;
+    return skeletons;
+}
+
+void battle(int humans, int skeletons) {
+    //battle program
+    //human fights skeleton
+    
+    //random number things
     
     unsigned int tiempo;
     
     tiempo = (unsigned int) time (NULL);
     
-    cout << tiempo << endl;
-    
-    //random number things
-    mt19937 randomGenerator (tiempo);
+    static mt19937 randomGenerator (tiempo);
     uniform_int_distribution<int> diceRoll (1, 6);
     uniform_real_distribution<float> attackRoll (0.0f, 1.0f);
     
-    cout << "interface\n";
-    //interface
-    //intro
-    //define number of skelly and humans
-    
-    cout << "Hello, welcome to humans vs skelletons.\n";
-    cout << "Please select the number of humans.\n";
-    cin >> humans;
-    cout << "Now, select the number of skelletons they will fight against.\n";
-    cin >> skelletons;
-    
-    cout << humans <<" humans will fight against " << skelletons << " skelletons.\n";
-    cout << "Let the battle start!!!\n";
-    
-    
-    //battle program
-    //human fights skelleton
     
     //stats
     
@@ -52,20 +96,21 @@ int main()
     int humanArmor = 20;
     float humanMissRate = 0.1f;
     
-    int skelletonHealth = 100;
-    int skelletonDmg = 50;
-    int skelletonArmor = 2;
-    float skelletonMissRate = 0.2f ;
+    int skeletonHealth = 100;
+    int skeletonDmg = 50;
+    int skeletonArmor = 2;
+    float skeletonMissRate = 0.2f ;
     
     int round = 1;
     
     cout << "battle start\n";
-    while (humans  >= 1 && skelletons >= 1) {    //loop till one team has 0 left
-        
+    
+    
+    while (humans  >= 1 && skeletons >= 1) {    //loop till one team has 0 left
         
         //turn based fight
         
-        while (humanHealth > 0 && skelletonHealth > 0) {    //loop till winner
+        while (humanHealth > 0 && skeletonHealth > 0) {    //loop till winner
             
             //iniciative roll
             int initiative = 1;
@@ -82,40 +127,40 @@ int main()
                 cout << "attack " << attack <<endl;
                 cout << "human won first roll\n";
                 if (attack > humanMissRate)   {
-                    skelletonHealth = skelletonHealth - humanDmg;
-                    cout << "human hits skelleton\n";
-                    cout << "skelletonHealth " << skelletonHealth <<endl;
+                    skeletonHealth = skeletonHealth - humanDmg;
+                    cout << "human hits skeleton\n";
+                    cout << "skeletonHealth " << skeletonHealth <<endl;
                 }
                 
-                if (skelletonHealth > 0) {      //if alive, skelleton attack if he lost iniciative
+                if (skeletonHealth > 0) {      //if alive, skeleton attack if he lost iniciative
                     attack = attackRoll (randomGenerator);
-                    cout << "skelleton is still alive\n";
-                    if (attack > skelletonMissRate)  {
-                        humanHealth = humanHealth - skelletonDmg;
-                        cout << "skelleton hits human\n";
+                    cout << "skeleton is still alive\n";
+                    if (attack > skeletonMissRate)  {
+                        humanHealth = humanHealth - skeletonDmg;
+                        cout << "skeleton hits human\n";
                         
                     }
                     
                 }
                 
-            } else {                            //skelleton attack if he won iniciative
+            } else {                            //skeleton attack if he won iniciative
                 attack = attackRoll (randomGenerator);
-                cout << "skelleton won first roll\n";
-                if (attack > skelletonMissRate)  {
-                    humanHealth = humanHealth - skelletonDmg;
-                    cout << "skelleton hits human\n";
+                cout << "skeleton won first roll\n";
+                if (attack > skeletonMissRate)  {
+                    humanHealth = humanHealth - skeletonDmg;
+                    cout << "skeleton hits human\n";
                 }
                 if (humanHealth > 0) {          //if alive, human attack if he lost iniciative
                     attack = attackRoll (randomGenerator);
                     cout << "human is still alive\n";
                     if (attack > humanMissRate)   {
-                        skelletonHealth = skelletonHealth - humanDmg;
-                        cout << "human hits skelleton\n";
+                        skeletonHealth = skeletonHealth - humanDmg;
+                        cout << "human hits skeleton\n";
                     }
                     
                 }
                 
-                cout << "humanHealth " << humanHealth << " skelletonHealth " << skelletonHealth << endl;
+                cout << "humanHealth " << humanHealth << " skeletonHealth " << skeletonHealth << endl;
             }
             
             //check for casualty and substract from total
@@ -126,34 +171,34 @@ int main()
                     humanHealth = 500;
                 }
             }
-            if (skelletonHealth <= 0){
-                skelletons--;
-                if (skelletons > 0) {
-                skelletonHealth = 150;
+            if (skeletonHealth <= 0){
+                skeletons--;
+                if (skeletons > 0) {
+                    skeletonHealth = 150;
                 }
-
+                
             }
             round ++;
-            cout << "there are " << humans << " humans and " << skelletons << " skelletons remaining.\n";
+            cout << "there are " << humans << " humans and " << skeletons << " skeletons remaining.\n";
         }
     }
     
     //game over
     
     cout << "Battle is over!\n";
+}
+
+void printResults(int humans, int skeletons) {
     
-    //outro
     //print results
-    
     
     if (humans > 0) {
         cout << "Humans have won!\n";
     } else {
-        cout << "Skelletons have won!\n";
+        cout << "Skeletons have won!\n";
         
     }
     
-    exit (0);
     
 }
 

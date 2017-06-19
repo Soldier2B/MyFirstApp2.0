1bf41cc28ac51bb69322fae5a43d1a95bd264003
Oliver Klein <Oliver@Olivers-MacBook-Pro.local>
2017-06-19 19:45:54 -0300

    Tryng to do object

--- MyFirstApp2.0/main2.cpp
+++ MyFirstApp2.0/main2.cpp
@@ -11,13 +11,15 @@ using namespace std;
 
 //Forward Declarations
 
-string getClass();
+string getClass(string playerClass);
 
 //Classes
 
 class Player {
     
 public:
+
+// player starting stats setter
     
     void setStartingStats (string playerClass) {
         
@@ -25,9 +27,12 @@ public:
             _playerHealth = 200;
             _playerDamage = 10;
             _playerMagicDmg = 50;
-            _playerAccuracy = 0;
+            _playerAccuracy = 0.0;
             _playerArmor = 0;
             _playerMagicDef = 0;
+            _playerSpeed = 0.0;
+            
+            cout << "Stats set to " << playerClass << " class.\n";
             
         }
         
@@ -38,24 +43,78 @@ public:
             _playerAccuracy = 0;
             _playerArmor = 0.0;
             _playerMagicDef = 0;
+            _playerSpeed = 0.0;
+            
+            cout << "Stats set to " << playerClass << " class.\n";
+
             
         }
         
     }
     
-    int Attack () {
+// In-Game stat setters
+    
+    void setPlayerHealth (int playerHealth) {
+        _playerHealth = playerHealth;
+    }
+    
+    void setPlayerDamage (int playerDamage) {
+        _playerDamage = playerDamage;
+    }
+    
+    void setPlayerMagicDmg (int playerMagicDmg) {
+        _playerMagicDmg = playerMagicDmg;
+    }
+    
+    void setPlayerAccuracy (int playerAccuracy) {
+        _playerAccuracy = playerAccuracy;
+    }
+    
+    void setPlayerArmor (int playerArmor) {
+        _playerArmor = playerArmor;
+    }
+    
+    void setPlayerMagicDef (int playerMagicDef) {
+        _playerMagicDef = playerMagicDef;
+    }
+    
+    void setPlayerSpeed (int playerSpeed) {
+        _playerSpeed = playerSpeed;
+    }
+    
+
+//In-Game getters
+    
+// player speed / initiative
+    
+    int getPlayerSpeed(){
+        return _playerSpeed;
+        
+    }
+// player attack
+    int getPlayerAttack () {
         int damageDealt = 0;
         return damageDealt;
     }
-    int Defend () {
+    int getPlayerMagicAttack () {
+        int magicDmgDealt = 0;
+        return magicDmgDealt;
+    }
+    
+// player defence
+    
+    int getPlayerDefence () {
         int damageMitigation = 0;
         return damageMitigation;
         
     }
-    int useExp () {
-        int remainingExp = 0;
-        return remainingExp;
+    int getPlayerMagicDef () {
+        int magicDmgMitigation = 0;
+        return magicDmgMitigation;
     }
+    
+    
+// player stats
     
 private:
     int _playerHealth;
@@ -64,6 +123,7 @@ private:
     float _playerAccuracy;
     int _playerArmor;
     int _playerMagicDef;
+    float _playerSpeed;
     
 };
 
@@ -72,11 +132,12 @@ private:
 
 int main () {
     
-    string playerClass;
-    playerClass = getClass ();
+    string playerClass = "unselected";
+    playerClass = getClass (playerClass);
     
     Player player1;
     player1.setStartingStats(playerClass);
+    
     
     
 
@@ -96,24 +157,30 @@ int main () {
 
 //Select class
 
-string getClass () {
+string getClass (string playerClass) {
     
     int selectClass;
-    string playerClass;
     
-    cout << "Select what class you want to play.\n";
-    cout << "{1} Mage  {2} Warrior \n";
-    cin >> selectClass;
+    while (playerClass == "unselected") {
     
-    //if selectClass = 1 mage
-    if (selectClass == 1) {
-        playerClass = "Mage";
-    }
+        cout << "Select what class you want to play.\n";
+        cout << "{1} Mage  {2} Warrior {3} More Info\n";
+        cin >> selectClass;
     
-    //if selectClass = 2 warrior
+        //if selectClass = 1 mage
+        if (selectClass == 1) {
+            playerClass = "Mage";
+        }
     
-    if (selectClass == 2){
-        playerClass = "Warrior";
+        //if selectClass = 2 warrior
+    
+        if (selectClass == 2){
+            playerClass = "Warrior";
+        }
+    
+        if (selectClass == 3) {
+            cout << "Warriors are the best, pick them.\n";
+        }
     }
     
     cout << "Player selected " << playerClass << endl;
@@ -152,6 +219,13 @@ string getClass () {
 
 
 //Exp Options
+
+// player exp managment
+
+/*  int useExp () {
+ int unusedExp = 0;
+ return unusedExp;
+ } */
 
 
 //Class types
